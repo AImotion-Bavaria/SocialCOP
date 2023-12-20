abf2acd88ed6fd86de360c36263313bc9804ccf3
Alexander Schiendorfer <alexander.schiendorfer@thi.de>
2023-12-20 17:09:39 +0100

    Rawls - assignment based

--- src/runners/rawls_runner_assignment.py
+++ src/runners/rawls_runner_assignment.py
@@ -1,7 +1,7 @@
 from minizinc import Instance, Model, Solver
 import re
 
-model = Model("src/mzn_models/rawls_test_table_python.mzn")
+model = Model("src/mzn_models/rawls_test_table_python_assignment.mzn")
 model.add_file("src/mzn_models/rawls2.dzn", parse_data=True)
 # Find the MiniZinc solver configuration for Gecode
 gecode = Solver.lookup("gecode")
@@ -11,21 +11,20 @@ instance = Instance(gecode, model)
 #Init counter, dimension and minimal_value array
 counter = 1
 dimension = model["dimension"]
-min_val = [0] * dimension
+maxmin_values = []
 
 #iterate for number of agents
 while(counter <= dimension):
     with instance.branch() as inst:
         #solve with given preconditions
-        inst["index"] = counter
-        inst["minimal_values"] = min_val
+        inst["maxmin_values"] = maxmin_values
         result = inst.solve()
 
         print(result)
 
         #calculate and store preconditions for next iteration 
-        worst_util = result["worst_util"]# int(re.search(r'worst_util=(\d+)', str(result)).group(1))
-        min_val[counter-1]=worst_util
+        worst_util = result["worst_util"]
+        maxmin_values.append(worst_util)
         counter = counter + 1
 
    
