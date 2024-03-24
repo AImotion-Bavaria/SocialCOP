0ca41694d002c436b4515ba9ac294ee912338bd3
rttmnn <jur9766@thi.de>
2024-03-24 11:42:27 +0100

    added pareto_runner for plain_tabular, changed pareto_runner to use json structure

--- src/runners/pareto_runner.py
+++ src/runners/pareto_runner.py
@@ -3,22 +3,19 @@ from minizinc import Model, Solver, Instance
 import sys 
 import os
 
-from utilitarian_runner import add_utilitarian_objective, optimize_utilitarian_objective 
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 from functools import partial
 
 from util.social_mapping_reader import read_social_mapping, AGENTS_ARRAY, UTILITY_ARRAY, NUM_AGENTS
 
 
-def add_proportionality_objective(social_mapper, instance : Instance):
+def add_pareto_objective(social_mapper, instance : Instance):
     instance.add_string(f"int: m = card(index_set_1of2(possible_solutions));")
-    instance.add_string(f"array[1..{social_mapper[NUM_AGENTS]}] of var float: max_values;")
-    instance.add_string(f"array[1..{social_mapper[NUM_AGENTS]}] of var int: selected;")
-    instance.add_string(f"constraint forall (p in 1..{social_mapper[NUM_AGENTS]}) (max_values[p] = max([possible_solutions[i, p] | i in 1..m]) / {social_mapper[NUM_AGENTS]});")
-    instance.add_string(f"constraint forall (p in 1..{social_mapper[NUM_AGENTS]}) (selected[p] >= max_values[p]);")
-    instance.add_string(f"constraint exists(i in 1..m) (forall(j in 1..{social_mapper[NUM_AGENTS]}) (selected[j] = possible_solutions[i, j]));")
+    instance.add_string(f"constraint exists(j in 1..m) (forall(i in 1..{social_mapper[NUM_AGENTS]}) (utilities[i] = possible_solutions[j, i]));")
+    instance.add_string(f"constraint not exists(j in 1..m) (sum(i in 1..{social_mapper[NUM_AGENTS]}) (bool2int(utilities[i] == possible_solutions[j, i]))+sum(i in 1..{social_mapper[NUM_AGENTS]}) (bool2int(utilities[i] < possible_solutions[j, i]))=={social_mapper[NUM_AGENTS]}/\ sum(i in 1..{social_mapper[NUM_AGENTS]}) (bool2int(utilities[i] < possible_solutions[j, i]))>0);")
 
-def optimize_proportionality_objective(instance : Instance):
+def optimize_pareto_objective(instance : Instance):
     instance.add_string(f"solve satisfy;")
     
 '''
@@ -48,10 +45,10 @@ if __name__ == "__main__":
 
 
     simple_runner = ProportionalityRunner(social_mapping)
-    simple_runner.add_presolve_handler(partial(add_proportionality_objective, social_mapping))
-    simple_runner.add_presolve_handler(optimize_proportionality_objective)
-    simple_runner.add_presolve_handler(partial(add_utilitarian_objective, social_mapping))
-   # simple_runner.add_presolve_handler(optimize_utilitarian_objective)
+    simple_runner.add_presolve_handler(partial(add_pareto_objective, social_mapping))
+    simple_runner.add_presolve_handler(optimize_pareto_objective)
+    #simple_runner.add_presolve_handler(partial(add_utilitarian_objective, social_mapping))
+    #simple_runner.add_presolve_handler(optimize_utilitarian_objective)
     result = simple_runner.run(plain_tabular_model, gecode)
     print(result)
 
