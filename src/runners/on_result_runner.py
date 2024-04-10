f5ec3924f8d2c51bc3894dd4f239f70ef6d876fc
Alexander Schiendorfer <alexander.schiendorfer@thi.de>
2024-04-10 15:45:37 +0200

    Generic on result runner

---
+++ src/runners/on_result_runner.py
@@ -0,0 +1,66 @@
+from simple_runner import SimpleRunner
+from minizinc import Model, Solver, Instance, Status 
+import sys 
+import os 
+import logging
+from functools import partial
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+from util.social_mapping_reader import read_social_mapping, AGENTS_ARRAY, UTILITY_ARRAY, NUM_AGENTS
+from util.mzn_debugger import create_debug_folder, log_and_debug_generated_files
+
+"""
+An on_result_runner implements tree search (Branch-and-Bound)
+where on having a result from a previous search, new constraints get posted
+"""
+
+class OnResultRunner(SimpleRunner):
+    def __init__(self, social_mapping) -> None:
+        super().__init__(social_mapping)
+        self.on_result_handlers = [] # a list of functions applied after seeing a result
+
+    def add_on_result_handler(self, handler):
+        self.on_result_handlers.append(handler)
+
+    def solve(self, inst: Instance):
+        res = inst.solve()
+        previous_solutions = [res]
+                 
+        solutions = []
+        while res.status == Status.SATISFIED:
+            solutions.append(res)
+            logging.info(f"Found solution: {res}") 
+
+            with inst.branch() as child:
+                for on_result_handler in self.on_result_handlers:
+                    on_result_handler(child, res)
+                if self.debug:
+                    log_and_debug_generated_files(child)
+                res = child.solve()
+        return previous_solutions
+
+if __name__ == "__main__":    
+    logging.basicConfig(level=logging.INFO)
+    debug_dir = create_debug_folder(os.path.dirname(__file__))
+    social_selection_model = Model(os.path.join(os.path.dirname(__file__), '../models/social_selection/social_selection.mzn'))
+    n_agents = 5
+    # we can also set some parameters to the Model instance right here in Python (or read from dzh)
+    social_selection_model["m"] = 3
+    social_selection_model["n"] = n_agents 
+    social_selection_model["weights"] = [1+i for i in range(n_agents)]
+    # Find the MiniZinc solver configuration for Gecode
+    gecode = Solver.lookup("gecode")
+    
+    # now let's read the social mapping file 
+    social_mapping_file = os.path.join(os.path.dirname(__file__), '../models/social_selection/social_mapping.json')
+    social_mapping = read_social_mapping(social_mapping_file)
+
+    # solve the same model using an on result runner 
+    on_result_runner = OnResultRunner(social_mapping)
+    from utilitarian import add_utilitarian_objective, get_better_utilitarian
+    # insert all decision variables to calculate the utilitarian objective for this model
+    on_result_runner.add_presolve_handler(partial(add_utilitarian_objective, social_mapping))
+    # optimize for the utilitarian objective using on result constraints
+    on_result_runner.add_on_result_handler(get_better_utilitarian)
+    on_result_runner.run(social_selection_model)
--- src/runners/utilitarian.py
+++ src/runners/utilitarian.py
@@ -1,8 +1,9 @@
 from simple_runner import SimpleRunner
-from minizinc import Model, Solver, Instance
+from minizinc import Model, Solver, Instance, Result
 import sys 
 import os 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from on_result_runner import OnResultRunner
 from functools import partial
 
 from util.social_mapping_reader import read_social_mapping, AGENTS_ARRAY, UTILITY_ARRAY
@@ -14,7 +15,11 @@ def add_utilitarian_objective(social_mapper, instance : Instance):
 
 def  optimize_utilitarian_objective(instance : Instance):
     instance.add_string(f"solve maximize {UTILITARIAN_OBJECTIVE};")
-    
+
+def get_better_utilitarian(instance : Instance, res : Result):
+    # enforce that the next solution needs to be better than the current one
+    instance.add_string(f"constraint {UTILITARIAN_OBJECTIVE} > {res[UTILITARIAN_OBJECTIVE]};")
+
 if __name__ == "__main__":
     import os
     social_selection_model = Model(os.path.join(os.path.dirname(__file__), '../models/social_selection/social_selection.mzn'))
@@ -37,3 +42,10 @@ if __name__ == "__main__":
     simple_runner.add_presolve_handler(optimize_utilitarian_objective)
     result = simple_runner.run(social_selection_model, gecode)
     print(result["selected"])
+
+    # solve the same model using an on result runner 
+    on_result_runner = OnResultRunner()
+    # insert all decision variables to calculate the utilitarian objective for this model
+    on_result_runner.add_presolve_handler(partial(add_utilitarian_objective, social_mapping))
+    # optimize for the utilitarian objective using on result constraints
+    on_result_runner.add_on_result_handler(optimize_utilitarian_objective)
