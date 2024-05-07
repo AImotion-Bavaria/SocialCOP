66b7cd45468557f4582f002ea32e78351c2a3eca
rttmnn <jur9766@thi.de>
2024-05-07 08:38:25 +0200

    refactoring evaluate_utilitarian_leximin.py

--- src/experiments/cluster_results/evaluate_utilitarian_leximin.py
+++ src/experiments/cluster_results/evaluate_utilitarian_leximin.py
@@ -2,11 +2,11 @@
 # for the comparison between leximin and utilitarian
 import json
 import os
-
 import sqlite3
 import os
 import numpy as np
 
+#see https://github.com/oliviaguest/gini
 def calculate_gini(array):
     array = np.sort(np.array(array)) #cast to sorted numpy array
     index = np.arange(1,array.shape[0]+1) #index per array element
@@ -32,28 +32,10 @@ with sqlite3.connect(db_file) as conn:
     leximin_utility_vector=[row[2] for row in rows]
     utilitarian_utility_vector=[row[3] for row in rows]
 
-def print_procentual_change():
-    utility_quotients=[]
-    for element in range(len(leximin_utility_vector)):
-        leximin_utility = json.loads(leximin_utility_vector[element])
-        utilitarian_utility = json.loads(utilitarian_utility_vector[element])
-        for agent in range(len(leximin_utility)):
-            #issue: -20:10 is a lower percentual increase then -10:10
-            if(utilitarian_utility[agent]!=0):
-                #procentual change is not devined for negative reference values
-                utility_quotients.append(((utilitarian_utility[agent])-(leximin_utility[agent]))/abs(utilitarian_utility[agent]))
-            else:
-                #division by zero not applicable
-                utility_quotients.append(np.nan)
-        avg = np.average(utility_quotients)
-        print("column", element, ":",  utility_quotients, "average: ", avg)
-        utility_quotients=[]
-
-
-
-
+#tables to store both results for every column
 gini_results_leximin=[]
 gini_results_utilitarian=[]
+#for every result
 for element in range(len(leximin_utility_vector)):
     leximin_utility = json.loads(leximin_utility_vector[element])
     utilitarian_utility = json.loads(utilitarian_utility_vector[element])
@@ -62,14 +44,4 @@ for element in range(len(leximin_utility_vector)):
     print("Gini at column: ",element,"leximin:",gini_results_leximin[element],"utilitarian:",gini_results_utilitarian[element], "difference:",gini_results_utilitarian[element]-gini_results_leximin[element])  
 print("Mean values: difference:", np.average([(b - a) for a, b in zip(gini_results_leximin, gini_results_utilitarian)]), "; average procentual change:", np.average([(b-a)/b if b != 0 else 0 for a, b in zip(gini_results_leximin, gini_results_utilitarian)]))
 
-
-
-
-    
-
-# Calculate the geometric mean of the "quotient" column
-#geometric_mean = np.prod(quotients) ** (1 / len(quotients))
-
-# Print the geometric mean
-#print("Geometric Mean of 'quotient' column:", geometric_mean)
 
