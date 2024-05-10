1f7fbf6d59ac9b06aea060ef8740c6a4ac8078b4
Alexander Schiendorfer <alexander.schiendorfer@thi.de>
2024-05-10 13:28:43 +0200

    Tables

--- src/experiments/cluster_results/extract_leximin_utilitarian_results.py
+++ src/experiments/cluster_results/extract_leximin_utilitarian_results.py
@@ -22,5 +22,33 @@ with sqlite3.connect(db_file) as conn:
     df = pd.read_sql_query(sql_query, conn)
 
 print(df.head(5))
-    
-print(df.to_latex(index=False, escape=False))
+df = df.drop(columns="model")    
+
+translations = {
+    'bus_tour_1.dzn': 'bus tour 1',
+    'bus_tour_3.dzn': 'bus tour 2',
+    'bus_tour_2.dzn': 'bus tour 3',
+    'bus_tour_4.dzn': 'bus tour 4',
+    'bus_tour_5.dzn': 'bus tour 5',
+    'generic_1.dzngeneric_1_preferences.dzn': 'tables 1',
+    'generic_2.dzngeneric_2_preferences.dzn': 'tables 2',
+    'generic_3.dzngeneric_3_preferences.dzn': 'tables 3',
+    'generic_4.dzngeneric_4_preferences.dzn': 'tables 4',
+    'generic_5.dzngeneric_5_preferences.dzn': 'tables 5',
+    'scheduling_1.dzn': 'scheduling 1',
+    'scheduling_2.dzn': 'scheduling 2',
+    'scheduling_4.dzn': 'scheduling 3',
+    'scheduling_5.dzn': 'scheduling 4',
+    'scheduling_6.dzn': 'scheduling 5',
+    'project_assignment_1.dzn': 'projects 1',
+    'project_assignment_2.dzn': 'projects 2',
+    'project_assignment_3.dzn': 'projects 3',
+    'project_assignment_4.dzn': 'projects 4'
+}
+df = df.replace({"data_files" : translations})
+for gini in ["gini_leximin", "gini_rawls", "gini_utilitarian"]:
+    df[gini] = df[gini].round(2)
+print(df.to_latex(index=False, escape=False, float_format="%.2f"))
+print(df["data_files"].tolist())
+
+
--- src/experiments/cluster_results/write_main_table.sql
+++ src/experiments/cluster_results/write_main_table.sql
@@ -1,5 +1,5 @@
 SELECT model, data_files, 
-       min_leximin, max_leximin, sum_utility_leximin, gini_leximin,
+       min_utilitarian, max_utilitarian, sum_utility_utilitarian, gini_utilitarian,
 	   min_rawls, max_rawls, sum_utility_rawls, gini_rawls,
 	   min_leximin, max_leximin, sum_utility_leximin, gini_leximin
  FROM Results
